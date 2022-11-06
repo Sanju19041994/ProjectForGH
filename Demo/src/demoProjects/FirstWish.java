9fab7f323d9a9e027a9c4b9f522f309073370686
Sanju19041994 <Sanjuchakole.sanju@gmail.com>
2022-11-06 10:08:12 +0530

    Changes Done

---
+++ Demo/src/demoProjects/FirstWish.java
@@ -0,0 +1,13 @@
+package demoProjects;
+
+public class FirstWish {
+
+	public static void main(String[] args) {
+		
+		System.out.println("git init");
+		System.out.println("git add <fileName> : for 1 file");
+		System.out.println("git add --a : for all");
+		System.out.println("git push");
+		
+	}
+}
